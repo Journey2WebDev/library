f873ba7a3f80f8aed3c39b0f64ddde8529bccec0
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-29 14:25:36 -0400

    Add .gitignore file

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+
+let myLibrary = [];
+
+function Book(title, author, haveRead, numPages) {
+  // our object constructor
+  this.title = title
+  this.author = author
+  this.haveRead = haveRead
+  this.numPages = numPages
+  // category, rating (0 to 5 stars)
+}
+
+book1 = new Book("curious george", "some guy", true, 50);
+console.log(book1);
+
+
+function addBookToLibrary() {
+  // leet code here
+}
+
+
+
