c39c9c20b544f6321ae6853c07272c0cf3a9d10c
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-29 21:02:03 -0400

    Add template grid for library books; add template form for book addition

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,22 @@
 
 let myLibrary = [];
 
-function Book(title, author, haveRead, numPages) {
+function Book(title, author, genre, haveRead, rating, numPages) {
   // our object constructor
   this.title = title
   this.author = author
+  this.genre = genre
   this.haveRead = haveRead
+  this.rating = rating
   this.numPages = numPages
-  // category, rating (0 to 5 stars)
 }
 
-book1 = new Book("curious george", "some guy", true, 50);
-console.log(book1);
-
+// Example Books
+let book1 = new Book("curious george", "some guy", "children", true, 4.5, 50);
+let book2 = new Book("curious george", "some guy", "children", true, 4.5, 50);
+let book3 = new Book("curious george", "some guy", "children", true, 4.5, 50);
+let book4 = new Book("curious george", "some guy", "children", true, 4.5, 50);
+let book5 = new Book("curious george", "some guy", "children", true, 4.5, 50);
 
 function addBookToLibrary() {
   // leet code here
