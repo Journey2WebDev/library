eaaa9681bee8cb75431235a4d9c3782eb75f2564
Journey2WebDev <MaxwellHandy@gmail.com>
2022-07-04 14:15:14 -0400

    Submit btn click moves form data to JS obj

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,14 @@
 
 let myLibrary = [];
 
-// Book object constructor
-function Book(title, author, genre, haveRead, rating, numPages) {
-  this.title = title
-  this.author = author
-  this.genre = genre
-  this.haveRead = haveRead
-  this.rating = rating
-  this.numPages = numPages
-}
-
-// Example Books
-// let book1 = new Book("curious george", "some guy", "children", true, 4.5, 50);
-// let book2 = new Book("curious george", "some guy", "children", true, 4.5, 50);
-// let book3 = new Book("curious george", "some guy", "children", true, 4.5, 50);
-// let book4 = new Book("curious george", "some guy", "children", true, 4.5, 50);
-// let book5 = new Book("curious george", "some guy", "children", true, 4.5, 50);
-
 function addBookToLibrary() {
   // leet code here
 }
 
+
+// 
+// Frist draft of needed features
+// 
 
 // Practice: Remove book from library
 let allCloseIcons = document.getElementsByClassName("icon-remove");
@@ -54,14 +41,46 @@ function toggleReadIcon(){
 
 
 // Practice: Onclick event for 'add new book' btn
-let newBookBtn = document.getElementById("btn-createForm");
-newBookBtn.addEventListener("click", showForm);
-
-// function showForm(){
-//   document.getElementById("main-form").style.display = "block";
-// }
+let showFormBtn = document.getElementById("btn-createForm");
+showFormBtn.addEventListener("click", showForm);
 
 function showForm(){
   document.getElementById("main-form").style.visibility = "visible";
   document.getElementById("main-form").style.opacity = "1";
 }
+
+
+// Practice: Onclick event to pass form data to book object
+// Book object constructor
+function Book(title, author, genre, numPages, haveRead, rating) {
+  this.title = title
+  this.author = author
+  this.genre = genre
+  this.numPages = numPages
+  this.haveRead = haveRead
+  this.rating = rating
+}
+
+// Example Books
+// let book1 = new Book("curious george", "some guy", "children", true, 4.5, 50);
+
+let addToLibraryBtn = document.getElementById("btn-addToLibrary");
+addToLibraryBtn.addEventListener("click", formDataToObject);
+
+function formDataToObject(){
+  newBook = new Book();
+
+  newBook.title = document.getElementById("bookTitle").value;
+  newBook.author = document.getElementById("bookAuthor").value;
+  document.getElementById("genreF").checked === true ? newBook.genre = "fiction" : newBook.genre = "non-fiction"
+  newBook.numPages = document.getElementById("numPages").value;
+  document.getElementById("haveReadY").checked === true ? newBook.haveRead = "have read" : newBook.haveRead = "have NOT read";
+  
+  return console.log(newBook);
+}
+
+// Practice: Create Book layout in Javascript
+
+
+
+
